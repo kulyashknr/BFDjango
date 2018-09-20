7e7caead103a9872edd0cc4fee1e5b5802732cc7
Kulyash Konyrova <konyrova@gmail.com>
2018-09-20 09:06:35 +0600

    To-List 2.0

--- Week3/todo/main/views.py
+++ Week3/todo/main/views.py
@@ -5,11 +5,11 @@ from datetime import datetime
 def todo_list(request):
 
 	context = {
-	'numbers': [i for i in range(0, 5)],
+	'num': [i for i in range(1, 5)],
 	'created': datetime.now(),
 	'status': False,
-	'owner': "Admin",
-	'due_on': datetime(2018, 9, 9)
+	'owner': "admin",
+	'due_on': datetime(2018, 9, 27)
 	}
 
 	return render(request, 'todo_list.html', context)
@@ -17,11 +17,11 @@ def todo_list(request):
 def completed_todo_list(request):
 
 	context = {
-	'numbers': [i for i in range(0, 3)],
+	'num': [i for i in range(1, 2)],
 	'created': datetime.now(),
 	'status': True,
-	'owner': "Admin",
-	'due_on': datetime(2018, 9, 9)
+	'owner': "admin",
+	'due_on': datetime(2018, 9, 27)
 	}
 
 	return render(request, 'completed_todo_list.html', context)
