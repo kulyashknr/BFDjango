85ca0b503b1da422635b20f8c8a517ffb369456e
Kulyash Konyrova <konyrova@gmail.com>
2018-10-06 14:40:23 +0600

    todo with CRUD, blog

---
+++ Week5/todo/main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+class List(models.Model):
+	name = models.CharField(max_length=200)
+	status = models.BooleanField(default=False)
+	create = models.DateTimeField(default=datetime.now, blank=True)
+	due = models.DateField('Date', blank=True, null=True)
+	
+	def __str__(self):
+		return self.name + ' | ' + str(self.create) + ' | ' + str(self.status) +' | ' + str(self.due)
